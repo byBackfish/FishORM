03c84caaeda54914785f113f061cd231ad39a77b
Maik Egbers <maik@bybackfish.de>
2024-04-20 15:03:09 +0200

    Add @AutoGenerated functionality to inserting

--- src/main/java/de/bybackfish/sql/core/DatabaseOptions.java
+++ src/main/java/de/bybackfish/sql/core/DatabaseOptions.java
@@ -3,6 +3,7 @@ package de.bybackfish.sql.core;
 public record DatabaseOptions(
         String host,
         int port,
+        String schema,
         String database,
         String username,
         String password
